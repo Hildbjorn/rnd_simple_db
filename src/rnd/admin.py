a6950e67f3391a93728b7426986052ad741674bc
Hildbjorn <hildbjorn@gmail.com>
2026-02-07 16:50:25 +0300

    2026-02-07

--- src/rnd/admin.py
+++ src/rnd/admin.py
@@ -1,11 +1,27 @@
 from django.contrib import admin
 
-from .models import RnD, RnDType
+from .models import Contract, ContractType, RnD, RnDType
+
+@admin.register(ContractType)
+class ContractTypeAdmin(admin.ModelAdmin):
+    model = ContractType
+    
+    list_display = ('short_name', 'name', 'description')
+
+@admin.register(Contract)
+class ContractAdmin(admin.ModelAdmin):
+    model = Contract
+    
+    list_display = ('type','number',)
 
 @admin.register(RnDType)
 class RnDTypeAdmin(admin.ModelAdmin):
     model = RnDType
+    
+    list_display = ('name', 'short_name', 'description')
 
 @admin.register(RnD)
 class RnDAdmin(admin.ModelAdmin):
     model = RnD
+    
+    list_display = ('type', 'code', 'title', 'contract')
--- src/rnd/models.py
+++ src/rnd/models.py
@@ -2,6 +2,66 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+class ContractType(models.Model):
+    """
+    Типы договоров.
+    """
+    
+    name = models.CharField(max_length=255,
+                                            verbose_name=_('Тип договора (полностью)'),
+                                            null=False,
+                                            blank=False,
+                                            help_text=_('Тип договора (полностью)')
+                                            )
+    
+    short_name = models.CharField(max_length=255,
+                                            verbose_name=_('Тип договора (кратко)'),
+                                            null=False,
+                                            blank=False,
+                                            help_text=_('Тип договора (кратко)')
+                                            )
+    
+    description = models.TextField(verbose_name='Краткое описание',
+                                                        null=True,
+                                                        blank=True
+                                                        )
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = _('Тип договора')
+        verbose_name_plural = _('Типы договоров')
+        ordering = ['id']
+
+
+class Contract(models.Model):
+    """
+    Основная модель для хранения информации о государственных контрактах, 
+    договорах с соисполнителями и дополнительных соглашениях к ним.
+    """
+    
+    type = models.ForeignKey(ContractType,
+                                            on_delete=models.CASCADE,
+                                            verbose_name='Тип ГК / договора',
+                                            null=True,
+                                            related_name='contract_type')
+    
+    number = models.CharField(max_length=255,
+                                            verbose_name=_('Номер ГК / договора'),
+                                            null=True,
+                                            blank=True,
+                                            help_text=_('Номер ГК / договора')
+                                            )
+    
+    def __str__(self):
+        return self.number
+    
+    class Meta:
+        verbose_name = _('Государственный контракт / Договор')
+        verbose_name_plural = _('Государственные контракты / Договоры')
+        ordering = ['number']
+
 class RnDType(models.Model):
     """
     Типы НИОКР согласно классификации.
@@ -60,6 +120,12 @@ class RnD(models.Model):
                                             verbose_name='Тип НИОКР',
                                             null=True,
                                             related_name='rnd_type')
+    
+    contract = models.ForeignKey(Contract,
+                                                on_delete=models.CASCADE,
+                                                verbose_name='ГК / договор',
+                                                null=True,
+                                                related_name='rnd_contract')
     
     def __str__(self):
         return self.code
