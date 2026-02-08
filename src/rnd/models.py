1c083c0541e03491364c1297e4ddba8eb1a0f45f
Hildbjorn <hildbjorn@gmail.com>
2026-02-08 09:30:23 +0300

    2026-02-08

--- src/rnd/models.py
+++ src/rnd/models.py
@@ -1,6 +1,3 @@
-"""
-Модели для системы управления контрактами и НИОКР.
-"""
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ValidationError
